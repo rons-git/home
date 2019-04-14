dd7a2fadba7c2d618b3aa242f43b047bb01f3074
rons-git <ron@cancunron.com>
2019-04-14 13:58:21 -0400

    update from windows

--- upd-capstone.sh
+++ upd-capstone.sh
@@ -3,5 +3,5 @@ rm -rf CarND-Capstone
 git clone https://github.com/rons-git/CarND-Capstone.git
 cd CarND-Capstone
 chmod a+x *.sh
-pip uninstall -y -r <(pip freeze)
+sudo pip uninstall -y -r <(pip freeze)
 pip install -r requirements.txt
