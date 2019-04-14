ba1b00229fe8eecb916ef23ea2e133d521a66a50
rons-git <ron@cancunron.com>
2019-04-14 15:18:31 -0400

    update from windows

--- upd-capstone.sh
+++ upd-capstone.sh
@@ -4,5 +4,6 @@ sudo apt install python3-pip
 git clone https://github.com/rons-git/CarND-Capstone.git
 cd CarND-Capstone
 chmod a+x *.sh
+hash -d pip
 pip install --upgrade pip
 pip install -r requirements.txt
