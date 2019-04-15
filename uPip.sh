e410c90afe0259cf157fa9c6d3a49f8c59b53195
rons-git <ron@cancunron.com>
2019-04-15 00:31:35 -0400

    update from windows

--- uPip.sh
+++ uPip.sh
@@ -3,7 +3,7 @@ echo broncos1 | sudo -S command
 sudo apt-get update -y
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 hash -d pip
 pip install --upgrade pip
 cd CarND-Capstone
--- uWebSocket.sh
+++ uWebSocket.sh
@@ -22,7 +22,7 @@ mkdir build
 cd build
 cmake ..
 make 
-sudo make install -y
+sudo make install
 cd ../..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
 sudo rm -r uWebSockets
