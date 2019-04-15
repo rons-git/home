36090d0add4a72de2b994b80d7171202c73a5b71
rons-git <ron@cancunron.com>
2019-04-15 03:19:42 -0400

    update from windows

--- uPip.sh
+++ uPip.sh
@@ -2,6 +2,4 @@ set -x
 echo broncos1 | sudo -S command
 sudo apt-get update -y
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-sudo python3 get-pip.py
-hash -d pip
-pip install --upgrade pip
+sudo -h python3 get-pip.py
