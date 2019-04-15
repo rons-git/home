0b228e88e2bb4527d448024ef7c64594bcd5fa66
rons-git <ron@cancunron.com>
2019-04-14 23:31:32 -0400

    update from windows

--- test.sh
+++
@@ -1,2 +0,0 @@
-echo broncos1 | sudo -S command
-y | bash <(wget -q -O - https://bitbucket.org/DataspeedInc/dbw_mkz_ros/raw/default/dbw_mkz/scripts/sdk_install.bash)
--- upd-sdc.sh
+++ upd-sdc.sh
@@ -5,6 +5,4 @@ git clone https://github.com/rons-git/sdc.git
 cd ~/sdc
 chmod a+x *.*
 chmod a+x *
-cd ..
-cd sdc
 
