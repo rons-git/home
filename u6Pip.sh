ce21eaf449cf7e7c08754e4e12dc54b1ecc34bd4
rons-git <ron@cancunron.com>
2019-05-05 18:15:23 -0400

    update from windows

--- u6Pip.sh
+++ u6Pip.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
-echo broncos1 | sudo -S command
-sudo apt-get update -y
+echo broncos1 | sudo -S apt-get update -y
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo -H python3 get-pip.py
 pip install --upgrade pip
--- u8Requirements.sh
+++ u8Requirements.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ex
-echo broncos1 | sudo -S command
 cd ScorpionDrive-System-Integration
-sudo -H pip install -r requirements.txt
+echo broncos1 | sudo -SH pip install -r requirements.txt
 sudo -H pip install --upgrade pillow
