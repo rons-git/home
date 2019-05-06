32928d59506a9bd2d3a52e10f0bd6871ffeba9ee
rons-git <ron@cancunron.com>
2019-05-05 23:08:25 -0400

    update from windows

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #Before running this script, reset ubuntu
+START_TIME=$SECONDS
 set -ex
-#./u1WebSocket.sh
-#./u2ROS.sh
-#./u3DBW.sh
+./u1WebSocket.sh
+./u2ROS.sh
+./u3DBW.sh
 ./u4Capstone.sh
 ./u5Yolo.sh
 ./u6Pip.sh
 ./u7Cuda.sh
 ./u8Requirements.sh
 cd ~/sdc
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec" 
