481767a059f854cc2262ab7c4e50cb2a81d45a44
rons-git <ron@cancunron.com>
2019-05-06 00:07:16 -0400

    update from windows

--- u2ROS.sh
+++ u2ROS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 echo broncos1 | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install ros-kinetic-desktop-full -y
 echo broncos1 | sudo -S rosdep init
