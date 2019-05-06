cd11cd8cae4dd3572beb3f60960e0aac12fa038f
rons-git <ron@cancunron.com>
2019-05-06 14:08:11 -0400

    update from windows

--- init.sh
+++ init.sh
@@ -2,14 +2,7 @@
 #Before running this script, reset ubuntu
 START_TIME=$SECONDS
 set -ex
-./u1WebSocket.sh
-./u2ROS.sh
-./u3DBW.sh
-./u4Capstone.sh
-./u5Yolo.sh
-./u6Pip.sh
-./u7Cuda.sh
-./u8Requirements.sh
+script -c "./u0All.sh" init-log.txt
 cd ~/sdc
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec" 
---
+++ u0All.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Before running this script, reset ubuntu
+set -ex
+./u1WebSocket.sh
+./u2ROS.sh
+./u3DBW.sh
+./u4Capstone.sh
+./u5Yolo.sh
+./u6Pip.sh
+./u7Cuda.sh
+./u8Requirements.sh
