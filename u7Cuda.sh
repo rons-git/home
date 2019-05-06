6ffee837f46cf0caae4b60b25a4409fe87ebacb7
rons-git <ron@cancunron.com>
2019-05-05 22:33:50 -0400

    update from windows

--- u7Cuda.sh
+++ u7Cuda.sh
@@ -2,6 +2,6 @@
 set -ex
 cd ~/sdc/ScorpionDrive-System-Integration/scripts
 echo broncos1 | sudo -S dpkg -i cuda-repo-ubuntu1604-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
 sudo apt-get update -y
 sudo apt-get install cuda
