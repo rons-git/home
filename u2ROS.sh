0719658268912d0e5a07e5185d4c4d5fce15d082
rons-git <ron@cancunron.com>
2019-05-05 20:46:09 -0400

    update from windows

--- u2ROS.sh
+++ u2ROS.sh
@@ -4,7 +4,7 @@ echo broncos1 | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(ls
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install ros-kinetic-desktop-full -y
-sudo rosdep init
+echo broncos1 | sudo -S rosdep init
 rosdep update -y
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 echo "cd ~/sdc" >> ~/.bashrc
