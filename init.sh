62a782208c9a01962263f1c5f748cae4ddcf075e
rons-git <ron@cancunron.com>
2019-05-06 16:06:00 -0400

    update from windows

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #Before running this script, reset ubuntu
 START_TIME=$SECONDS
 set -ex
-script -c "./u0All.sh" init-log.txt
+script -c "./u0All.sh" init.log
 cd ~/sdc
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec" 
