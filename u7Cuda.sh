e7728e8e8a3e5eac7dcc745fd260b1de89bc230a
rons-git <ron@cancunron.com>
2019-05-05 22:28:13 -0400

    update from windows

--- u7Cuda.sh
+++ u7Cuda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+cd scripts
 echo broncos1 | sudo -S dpkg -i cuda-repo-ubuntu1604-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
 sudo apt-get update -y
