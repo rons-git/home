06f906c60e15618bbdd2bc467ea4e328517193d7
rons-git <ron@cancunron.com>
2019-05-07 00:02:38 -0400

    update from windows

--- u0All.sh
+++ u0All.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Before running this script, reset ubuntu
 set -ex
-./u1WebSocket.sh
-./u2ROS.sh
-./u3DBW.sh
-./u4Capstone.sh
-./u5Yolo.sh
-./u6Pip.sh
-./u7Cuda.sh
+./u1Capstone.sh
+./u2Yolo.sh
+./u3Cuda.sh
+./u4WebSocket.sh
+./u5ROS.sh
+./u6DBW.sh
+./u7Pip.sh
 ./u8Requirements.sh
 ./r9Capstone.sh
